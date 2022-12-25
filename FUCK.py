619b71ca9e8728bbc01e2a011faa3e87784c9a8d
HA S AN <117574358+hasanraj720@users.noreply.github.com>
2022-12-25 21:04:51 +0600

    FUCKER On Fire
    🔥Assalamualaikum 🔥

---
+++ FUCK.py
@@ -0,0 +1,438 @@
+import os
+
+os.system('clear')
+
+os.system("pip install requests")
+
+os.system("xdg-open https://www.facebook.com/profile.php?id=100083056775554")
+
+def Zeek():
+
+    os.system('clear')
+
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100083056775554") 
+
+    os.system('clear')
+
+print("""
+
+  _______  __    __    ______  __  ___  _______ .______      
+ |   ____||  |  |  |  /      ||  |/  / |   ____||   _  \     
+ |  |__   |  |  |  | |  ,----'|  '  /  |  |__   |  |_)  |    
+ |   __|  |  |  |  | |  |     |    <   |   __|  |      /     
+ |  |     |  `--'  | |  `----.|  .  \  |  |____ |  |\  \----.
+ |__|      \______/   \______||__|\__\ |_______|| _| `._____|
+                                                             
+
+   ______   .__   __.     _______  __  .______       _______ 
+  /  __  \  |  \ |  |    |   ____||  | |   _  \     |   ____|
+ |  |  |  | |   \|  |    |  |__   |  | |  |_)  |    |  |__   
+ |  |  |  | |  . `  |    |   __|  |  | |      /     |   __|  
+ |  `--'  | |  |\   |    |  |     |  | |  |\  \----.|  |____ 
+  \______/  |__| \__|    |__|     |__| | _| `._____||_______|
+                                                             
+
+
+
+\033[1;97m
+
+\033[1;32m                          🔥FUCKER On Fire 🔥 
+
+\033[1;97m==============================================
+
+\033[32mNever recommend
+
+\033[97mNOTE ITS A COLLECTION OF SOME COMMANDS
+
+\033[32mAUTHOR :\033[32mFUCKER 
+
+\033[1;97mGITHUB : \033[32mFUCKER 
+
+\033[1;32mFb :  FUCKER 
+
+\033[1;32mANY PROBLEM WHATSAPP: NO WHAT'S APP 🤘
+
+\033[1;97mFUCKER_On_Fire 🔥🤘🔥
+
+\033[0;97m================================================================\n\033[1;32m TIME TO TAKE EXIT FROM SOCIAL MEDIA \n \033[1;97mALL IN ONE """)
+
+print("""
+
+\033[1;32m[1]CLONE BANGLADESH ID[fast]
+
+\033[1;32m[2]CLONE FROM BANGLADESH 6DIG[All SIM]
+
+\033[1;32m[3]CLONE FROM CLONE FROM ID BANGLADESH 9DIG (All SIM)
+
+\033[1;32m[5]CLONE FROM FREOM PAKISTAN
+
+\033[1;32m[6]CLONE FROM FROM INDIA
+
+\033[1;32m[7]CLONE FROM Thailand ID
+
+\033[1;32m[8]CLONE FROM AFGHANISTAN 
+
+\033[1;32m[9]CLONE FROM FREOM PAKISTAN V2(All SIM)
+
+\033[1;32m[10]CLONE FROM FREOM 4 COUNTRY 
+
+\033[1;32m[11]CLONE FROM FREOM all COUNTRY
+
+\033[1;32m[12]CLONE FROM INSTRAGRAM
+
+\033[1;32m[13]GAMING ID CLONER 
+
+\033[1;32m[14]CLONE FROM PUBLICK ID (need Cookie)
+
+\033[1;32m[15]CLONE DEATH YAHOO
+
+\033[1;32m[16]CLONE FROM 2006- 2009 ID[Yahoo]
+
+\033[1;32m[17]CLONE FROM 2009 ID [MAO]
+
+\033[1;32m[18]CLONE FROM 2006- 2009 ID [FB]
+
+\033[1;32m[19]FB AUTO SHARE (need Cookie)
+
+\033[1;32m[20]FB AUTO COMMENT(need Cookie)
+
+\033[1;97m  ___________🔥🖕🖕🖕🔥___________ onen
+
+\033[1;32m[21]CLONE FROM  PUBLICK ID  (Best)64 bid  🖕                
+
+\033[1;32m[22]CLONE FROM  PUBLICK ID  (Best)64 bid.    🖕
+
+\033[1;32m[23]CLONE FROM  PUBLICK ID  (Best)64 bid.   🖕
+
+\033[1;32m[24]CLONE FROM FRIENDLIST (34 bid)V1
+
+\033[1;32m[25]CLONE FROM FRIENDLIST (34 bid)v2
+
+\033[1;32m[26]CLONE FROM FRIENDLIST (34 bid)V3 
+
+\033[1;32m[27]CLONE FROM FRIENDLIST (34/64bid)V3 
+
+\033[1;32m[28]CLONE FROM FRIENDLIST v4
+
+""")
+
+pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+        os.system('git clone https://github.com/Fucker-MH/mall')
+
+        os.system('cd mall && python2 RR_Premium.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;97mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+elif pil in ["02", "2"]:
+
+        os.system('git clone https://github.com/FUCKER /paidfree4')
+
+        os.system('cd paidfree4 && python2 BD6.py')
+
+        
+
+        
+
+elif pil in ["3"]:
+
+        os.system('git clone https://github.com/FUCKER /shuvook.git && cd $HOME')
+
+        so,system('cd shuvook && python2 bd9.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+elif pil in ["04", "4"]:
+
+        os.system('git clone https://github.com/FUCKER /BD-CLONER')
+
+        os.system('cd BD-CLONER')
+
+        os.system('python2 Bd-Cloner-1.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+        Zeek()
+
+    
+
+elif pil in ["5"]:
+
+        os.system('git clone https://github.com/FUCKER /paidfree4 && cd $HOME')
+
+        os.system('cd paidfree4 && python2 pakistan.py')
+
+        time.sleep(2)
+
+    
+
+elif pil in ["6"]:
+
+        os.system('pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Azim-vau/clone-india.git ; cd clone-india ; python2 india.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)   
+
+    
+
+elif pil in ["7"]:
+
+        os.system('git clone https://github.com/FUCKER /mall')
+
+        os.system('cd mall && python2 Thailand.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+elif pil in ["8"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd paidfree4 && python2 Mahadi-Afg.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+        Zeek()
+
+    
+
+elif pil in ["9"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd paidfree4')
+
+        os.system('python fucker.py')
+
+        time.sleep(2)
+
+ 
+
+elif pil in ["10"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd pacman && python2 bolt')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+        Zeek()
+
+		
+
+elif pil in ["11"]:   
+
+        os.system('git clone http://github.com/nfs-tech-bd/NFS-ALL')
+
+        os.system('cd NFS-ALL && python2 NFS-ALL.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)	
+
+elif pil in ["12"]:
+
+        os.system('pip2 install requests mechanize')
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd paidfree4')
+
+        os.system('python2 instragam.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+        Zeek()
+
+    
+
+elif pil in ["13"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd pro-1 && python pro.py')
+
+        time.sleep(2)
+
+        print(" ")
+
+        n = input("[ \n\033[1;32mBACK \n\033[1;97m]")
+
+        time.sleep(2)
+
+        Zeek()
+
+elif pil in ["14"]:
+
+        os.system('goit clne https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd texs && python2 xm.py')
+
+        time.sleep(2)
+
+    
+
+elif pil in ["15"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')	
+
+        os.system('cd texs && python yahoo.py')
+
+elif pil in ["16"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd texs && python2 santo.py')
+
+        time.sleep(2)   
+
+elif pil in ["17"]:
+
+        os.system('rm -rf Old-Crack && git clone https://github.com/syedzada1100/Old-Crack.git')
+
+        os.system('cd Old-Crack && python2 Old.py')
+
+    
+
+elif pil in ["18"]:
+
+        os.system('git clone https://github.com/Fucker-MsM/paidfree4')
+
+        os.system('cd paidfree4 && python 20091st.py')
+
+        time.sleep(2)
+
+    
+
+elif pil in ["19"]:
+
+        os.system('git clone https://github.com/Fucker-MsM/fbboT')
+
+        os.system(cd fbboT && python fucker.py')
+
+elif pil in ["20"]:
+
+        os.system('git clone https://github.com/Fucker-MsM/fbboT')
+
+        os.system('cd fbboT && python autocomment.py')
+        
+
+elif pil in ["21"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        so.system('cd ADF && python MsM.py')
+
+    	
+
+elif pil in ["22"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd mall && python Fucker.py')
+
+        
+
+    
+
+	
+
+elif pil in ["23"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd mall && python fuckmax.py')
+
+    	
+
+elif pil in ["24"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd mall && python2 file.py')
+
+    
+
+elif pil in ["25"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git′)
+
+        os.system('cd Cookie-Gen && python2 Genrate.py')
+
+     
+
+elif pil in ["26"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd mall && python3 okk.py')
+
+        
+
+elif pil in ["27"]:
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd mall && python number.py')
+
+elif pil in ["28"]:
+
+        os.system('rm -rf MsM')
+
+        os.system('git clone https://github.com/hasanraj720/RAJ.git')
+
+        os.system('cd MsM && python HASAN.py')
+
+       
